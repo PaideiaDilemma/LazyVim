2692d87383359a45a8b16899a956ac35f31a0756
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-11-03 12:14:10 +0100

    remap p to P for visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
--- tabs
+--
+-- modes:
+--  "n" - normal mode
+--  "i" - insert mode
+--  "x" - visual mode
+--  "v" - visual and select mode
+
+vim.keymap.set("x", "p", "P", { silent = true })
