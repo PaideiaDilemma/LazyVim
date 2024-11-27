7131a49f6f87addda23e3073daaee8b97077a6e0
Maximilian Seidler <maximilian.seidler@soundwork.at>
2024-11-27 09:09:25 +0100

    improve mini.files config

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -107,11 +107,23 @@ return {
 
   {
     "echasnovski/mini.files",
+    opts = {
+      windows = {
+        preview = true,
+        width_focus = 30,
+        width_preview = 120,
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        use_as_default_explorer = false,
+      },
+    },
     keys = {
       {
         "<leader>o",
         function()
-          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+          require("mini.files").open(vim.api.nvim_buf_get_name(0) or vim.uv.cwd(), true)
         end,
         desc = "Open mini.files (Directory of Current File)",
       },
