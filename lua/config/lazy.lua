b566c875ff057e47fe0410f9eb4190914d727ee9
Maximilian Seidler <maximilian.seidler@soundwork.at>
2025-05-27 08:28:54 +0200

    add kulala

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.rest.kulala" },
+    { import = "lazyvim.plugins.extras.util.rest" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.dap.core" },
